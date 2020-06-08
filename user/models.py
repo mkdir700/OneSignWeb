faf068f15e3782f562de3b86ee110096a2436523
mkdir700 <mkdir700@gmail.com>
2020-06-08 16:27:30 +0800

    新增字段is_auto_sign

--- user/models.py
+++ user/models.py
@@ -11,17 +11,19 @@ class User(AbstractUser):
     # cookie
     cookie = models.TextField(default='')
     # 默认为null,第一次登录的时候才去设置
-    # date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
+    date_joined = models.DateTimeField(default=timezone.now, verbose_name="加入时间")
     cookie_expired_time = models.DateTimeField(verbose_name='cookie失效时间', default=datetime.datetime.fromtimestamp(
         timezone.now().timestamp() + 864000))
+    # cookie_expired_time = models.DateTimeField(verbose_name="cookie失效时间")
     # wxPushKey
     wxPushKey = models.CharField(max_length=50, default='', verbose_name='消息Key')
+    is_auto_sign = models.BooleanField(default=True, verbose_name='是否自动打卡')
 
     class Meta(AbstractUser.Meta):
         pass
 
     def __str__(self):
-        return self.tel
+        return '{}({})'.format(self.last_name if self.last_name else '??? ', self.username)
 
     # @staticmethod
     def check_verify_code(self, username, password):
@@ -35,11 +37,6 @@ class User(AbstractUser):
                                                  microseconds=-now.microsecond)
         return True if self.cookie_expired_time > zero_tomorrow else False
     cookie_is_valid.short_description = 'cookie是否有效'
-
-    def wx_push_is_bind(self) -> bool:
-        """用户是否绑定wxpush"""
-        return True if self.wxPushKey else False
-    wx_push_is_bind.short_description = '是否绑定wxPush'
 
     def update_cookie_expire_time(self):
         """更新cookie的失效时间"""
